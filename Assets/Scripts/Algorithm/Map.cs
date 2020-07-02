704ee7f23ff2e28531ba9d8b55c0e2717660144d
lightjiao <lightjiao@gmail.com>
2020-07-02 16:56:11 +0800

    :sparkles: (map): Implement of initObstacle

--- Assets/Scripts/Algorithm/Map.cs
+++ Assets/Scripts/Algorithm/Map.cs
@@ -5,10 +5,14 @@ namespace A_Star.Algorithm
 {
     public class Map : MonoBehaviour
     {
-        [SerializeField] private UI.Map uiGrid = null;
+        [SerializeField] private UI.Map gridUI = null;
+        private const int gridSize = 50;
+        private List<List<Node>> gridData;
 
-        private const int mapLength = 50;
-        private List<List<Node>> map;
+        private int obstacleNumber = gridSize / 8;
+        private List<(int x, int y)> obstacleList;
+        private int minObstacleLength = 3;
+        private int maxObstacleLength = gridSize / 4;
 
         private void Start()
         {
@@ -18,20 +22,46 @@ namespace A_Star.Algorithm
 
         private void initMap()
         {
-            map = new List<List<Node>>(mapLength);
-            for (int i = 0; i < mapLength; i++)
+            gridData = new List<List<Node>>(gridSize);
+            for (int i = 0; i < gridSize; i++)
             {
-                map.Add(new List<Node>(mapLength));
-                for (int j = 0; j < mapLength; j++)
+                gridData.Add(new List<Node>(gridSize));
+                for (int j = 0; j < gridSize; j++)
                 {
-                    map[i].Add(new Node(Node.NodeState.None, i, j));
+                    gridData[i].Add(new Node(Node.NodeState.None, i, j));
                 }
             }
         }
 
         private void initObstacle()
         {
-            // 
+            obstacleList = new List<(int x, int y)>();
+
+            // 初始化几个obstacle的端点
+            obstacleList.Add((gridSize / 2, gridSize / 2));
+            for (int i = 0; i < obstacleNumber - 1; i++)
+            {
+                int randX = Random.Range(0, gridSize);
+                int randY = Random.Range(0, gridSize);
+                obstacleList.Add((randX, randY));
+            }
+
+            // 根据obstacle端点生成连续的obstacles
+            foreach (var (x, y) in obstacleList)
+            {
+                int newX = x;
+                int newY = y;
+
+                int xAddition = 1;
+                int yAddition = Random.Range(-1, 2);
+                int obstacleLength = Random.Range(minObstacleLength, maxObstacleLength);
+                for (int i = 0; i < obstacleLength; i++)
+                {
+                    newX += xAddition;
+                    newY += yAddition;
+                    obstacleList.Add((newX, newY));
+                }
+            }
         }
     }
 }
